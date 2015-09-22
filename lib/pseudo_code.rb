f39a165ab267936251355184bed275b63ef1f73e
Amber Crawford <ammcrawford@gmail.com>
2015-09-22 13:12:11 -0600

    Added pseudo code file for notes

--- lib/pseudo_code.rb
+++ lib/pseudo_code.rb
@@ -1,4 +1,18 @@
 # Pseudocode
+  # Notes:
+    # Actually rethinking the giant hash.  Matt S. sent me the starting test that Josh gave them, here:
+      # class TestLoadingDistricts < Minitest::Test
+        #   def test_it_can_load_a_district_from_csv_data
+        #     dr = DistrictRepository.from_csv(data_dir)
+        #     district = dr.find_by_name("ACADEMY 20")
+        # ​
+        #     assert_equal 22620, district.enrollment.in_year(2009)
+        #     assert_equal 0.895, district.enrollment.graduation_rate.for_high_school_in_year(2010)
+        #     assert_equal 0.857, district.statewide_testing.proficient_for_subject_by_grade_in_year(:math, 3, 2008)
+        #   end
+        # end
+      # I think Torie's version of having each "instance" of a district create it's own hashes might be better.
+
   class DistrictRepository
     def self.from_csv(path)
       # opens one csv in folder
