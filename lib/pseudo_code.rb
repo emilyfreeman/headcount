7791212472bc01b5b61e9fc0aa7ae4043d75b116
Amber Crawford <ammcrawford@gmail.com>
2015-09-22 18:05:00 -0600

    Updated psuedo_code file

--- lib/pseudo_code.rb
+++ lib/pseudo_code.rb
@@ -8,27 +8,33 @@ require 'csv'
       # opens one csv in folder
       filename = "Students qualifying for free or reduced price lunch.csv"
       fullpath = File.join(path, filename)
-      district_repo = {}
+      @repository = {}
       # pushes it into parser
       rows = CSV.read(fullpath, headers: true, header_converters: :symbol).each do |row|
         # only push unique names into our hash and have them point to its instance of District
-        if !district_repo.include?(row[:location])
-          district_repo[row[:location]] = District.new(row[:location])
+        if !@repository.include?(row[:location])
+          @repository[row[:location]] = District.new(row[:location])
         end
       end
-      # returns hash containing just district names
-      district_repo
+      # returns hash containing just district names as strings
+      # binding.pry
+      self
     end
 
-    def find_by_name
-      # searches returned hash above for district name - returns instance of district
+    def self.find_by_name(name)
+      # searches returned hash above for district name; receives string; - returns instance of district
+      if @repository.keys.include? name
+        return @repository[name]
+      else
+        nil
+      end
     end
 
   end
 
 
   class District
-    # calls instances of "enrollment" and "statewide_testing" with argument of district name
+    # calls instances of "economic profile ", "enrollment" and "statewide_testing" with argument of district name
     def initialize(name)
       puts nil
     end
@@ -50,7 +56,7 @@ require 'csv'
   # etc...
 
 end
-
-path       = File.expand_path("../data", __dir__)
-repository = DistrictRepository.from_csv(path)
-puts repository.inspect
+#
+# path       = File.expand_path("../data", __dir__)
+# repository = DistrictRepository.from_csv(path)
+# puts repository.inspect
--- test/test_economic_profile.rb
+++ test/test_economic_profile.rb
@@ -1,38 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require 'CSV'
 require 'pry'
-
-
-class DistrictRepository
-
-  # receives a string
-  # returns a DistrictRepository
-  def self.from_csv(path) # this method should be the only self. method so that it can handle JSON data as well as CSV data
-    filename = "Students qualifying for free or reduced price lunch.csv"
-    fullpath = File.join(path, filename)
-    rows = CSV.read(fullpath, headers: true, header_converters: :symbol).each {|row| row.to_h} # remove first
-    repo_data = rows.each {|row| row[:location] = "ACADEMY 20"}
-    # DistrictRepository.new(repo_data)
-
-    # {location:
-                {ACADEMY 20:
-                            {
-                              
-                            }
-                {COLORADO:
-
-
-
-
-                }
-  end
-
-  def find_by_name(string)
-
-  end
-
-end
+require_relative '../lib/pseudo_code'
 
 class TestEconomicProfile < Minitest::Test
   def test_free_or_reduced_lunch_in_year
