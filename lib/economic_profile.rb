ced61a50a0414c5c61145aa9baf62d364007b904
Amber Crawford <ammcrawford@gmail.com>
2015-09-24 15:29:57 -0600

    Fixed missing elements

--- lib/economic_profile.rb
+++ lib/economic_profile.rb
@@ -3,70 +3,69 @@ require 'pry'
 
 class EconomicProfile
 
-    def initialize(district_name)
-      @district_name = district_name
-    end
+  def initialize(district_name)
+    @district_name = district_name
+  end
 
-    def free_or_reduced_lunch_by_year
-      filename = "Students qualifying for free or reduced price lunch.csv"
-      parsed_file = Parse.new(@district_name, filename).parse_runner
-      data = {}
-      parsed_file.each do |row|
-        if row.fetch(:poverty_level) == "Eligible for Free or Reduced Lunch" && row.fetch(:dataformat) == "Percent"
-          data[row.fetch(:timeframe).to_i] = row[:data].to_s[0..4].to_f
-        end
-        binding.pry
-      end
-      data
-    end
-
-    def free_or_reduced_lunch_in_year(year)
-      if free_or_reduced_lunch_by_year[year]
-        free_or_reduced_lunch_by_year.fetch(year)
-      else
-        nil
+  def free_or_reduced_lunch_by_year
+    filename = "Students qualifying for free or reduced price lunch.csv"
+    parsed_file = Parse.new(@district_name, filename).parse_runner
+    data = {}
+    parsed_file.each do |row|
+      if row.fetch(:poverty_level) == "Eligible for Free or Reduced Lunch" && row.fetch(:dataformat) == "Percent"
+        data[row.fetch(:timeframe).to_i] = row[:data].to_s[0..4].to_f
       end
     end
+    data
+  end
 
-    def school_aged_children_in_poverty_by_year
-      filename = "School-aged children in poverty.csv"
-      parsed_file = Parse.new(@district_name, filename).parse_runner
-      data = {}
-      parsed_file.each do |row|
-        if row.fetch(:dataformat) == "Percent"
-          data[row.fetch(:timeframe).to_i] = row[:data].to_s[0..4].to_f
-        end
-      end
-      data
+  def free_or_reduced_lunch_in_year(year)
+    if free_or_reduced_lunch_by_year[year]
+      free_or_reduced_lunch_by_year.fetch(year)
+    else
+      nil
     end
+  end
 
-    def school_aged_children_in_poverty_in_year(year)
-      if school_aged_children_in_poverty_by_year[year]
-        return school_aged_children_in_poverty_by_year.fetch(year)
-      else
-        return nil
-      end
-    end
-
-    def title_1_students_by_year
-        filename = "Title I students.csv"
-        parsed_file = Parse.new(@district_name, filename).parse_runner
-        data = {}
-        parsed_file.each do |row|
-          if row.fetch(:dataformat) == "Percent"
-            data[row.fetch(:timeframe).to_i] = row[:data].to_s[0..4].to_f
-          end
-        end
-      data
-    end
-
-    def title_1_students_in_year(year)
-      if title_1_students_by_year[year]
-        return title_1_students_by_year.fetch(year)
-      else
-        return nil
+  def school_aged_children_in_poverty_by_year
+    filename = "School-aged children in poverty.csv"
+    parsed_file = Parse.new(@district_name, filename).parse_runner
+    data = {}
+    parsed_file.each do |row|
+      if row.fetch(:dataformat) == "Percent"
+        data[row.fetch(:timeframe).to_i] = row[:data].to_s[0..4].to_f
       end
     end
+    data
+  end
+
+  def school_aged_children_in_poverty_in_year(year)
+    if school_aged_children_in_poverty_by_year[year]
+      return school_aged_children_in_poverty_by_year.fetch(year)
+    else
+      return nil
+    end
+  end
+
+  def title_1_students_by_year
+    filename = "Title I students.csv"
+    parsed_file = Parse.new(@district_name, filename).parse_runner
+    data = {}
+    parsed_file.each do |row|
+      if row.fetch(:dataformat) == "Percent"
+        data[row.fetch(:timeframe).to_i] = row[:data].to_s[0..4].to_f
+      end
+    end
+    data
+  end
+
+  def title_1_students_in_year(year)
+    if title_1_students_by_year[year]
+      return title_1_students_by_year.fetch(year)
+    else
+      return nil
+    end
+  end
 
 
 end
