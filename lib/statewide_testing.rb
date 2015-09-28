2ca6ef5b2ac0fcb0b84f15c0edf415a63dfe48d7
Emily Dowdle <emily@editingemily.com>
2015-09-28 13:15:11 -0600

    Working Statewide Testing class

--- lib/statewide_testing.rb
+++ lib/statewide_testing.rb
@@ -1,4 +1,5 @@
 require_relative 'parse'
+require_relative 'unknown_data_error'
 require 'pry'
 
 class StatewideTesting
@@ -15,13 +16,32 @@ class StatewideTesting
     Parse.new(@district_name, filename).parse_runner
   end
 
+  def confirm_race_data(race_sym)
+    race_data = [:asian, :black, :pacific_islander, :hispanic, :native_american, :two_or_more, :white, :all_students]
+    if !race_data.any?{ |race| race == race_sym }
+      raise UnknownRaceError
+    end
+  end
+
+  def confirm_grade_data(grade_num)
+    grade_data = [3, 8]
+    if !grade_data.any?{ |input| input == grade_num }
+      raise UnknownDataError
+    end
+  end
+
+  def confirm_subject_data(subject_sym)
+    subject_data = [:math, :reading, :writing]
+    if !subject_data.any?{ |subject| subject == subject_sym }
+      raise UnknownDataError
+    end
+  end
+
   def choose_data_for_testing_scores(grade)
     if grade == 3
       "3rd grade students scoring proficient or above on the CSAP_TCAP.csv"
-    elsif grade == 8
+    else grade == 8
       "8th grade students scoring proficient or above on the CSAP_TCAP.csv"
-    else
-      raise "WTF? You need a UnknownDataError class"
     end
   end
 
@@ -29,19 +49,22 @@ class StatewideTesting
     # grade is an integer; either 3 or 8
     # unknown grade should raise a UnknownDataError
     # The method returns a hash grouped by year referencing percentages by subject all as three digit floats.
+    confirm_grade_data(grade)
     parsed_file = parser(choose_data_for_testing_scores(grade))
 
     statewide_by_date = parsed_file.group_by {|hsh| hsh.fetch(:timeframe).to_i}
     statewide_by_date.each { |(k,v)| statewide_by_date[k] = v.each_with_object({}) { |score, hsh| hsh[ ( score[:score] ).to_sym.downcase ] = truncate_floats( score[:data] ) } }
   end
 
-  def filter_scores_on_race(scores, input)
-    scores.select { |score| score[:race_ethnicity].to_sym.downcase == input }
+  def filter_scores_on_race(scores, race)
+    # scores.select { |score| score[:race_ethnicity].to_sym.downcase == race }
+    scores.select { |score| score[:race_ethnicity].gsub("Hawaiian/Pacific Islander", "Pacific Islander").gsub(" ", "_").to_sym.downcase == race }
+
   end
 
   def retrieve_race_based_reading_scores(sym)
     parsed_reading_scores = parser( "Average proficiency on the CSAP_TCAP by race_ethnicity_ Reading.csv" )
-    filter_scores_on_race( parsed_reading_scores, sym )
+    parsed_file = filter_scores_on_race( parsed_reading_scores, sym )
   end
 
   def retrieve_race_based_writing_scores(sym)
@@ -54,16 +77,12 @@ class StatewideTesting
     filter_scores_on_race( parsed_math_scores, sym )
   end
 
-  def create_case_insensitive_symbol(str)
-    str.to_sym.downcase
-  end
-
   def proficient_by_race_or_ethnicity(queried_race)
-    input = create_case_insensitive_symbol(queried_race)
+    confirm_race_data(queried_race)
 
-    filtered_reading_scores = retrieve_race_based_reading_scores(input)
-    filtered_writing_scores = retrieve_race_based_writing_scores(input)
-    filtered_math_scores = retrieve_race_based_math_scores(input)
+    filtered_reading_scores = retrieve_race_based_reading_scores(queried_race)
+    filtered_writing_scores = retrieve_race_based_writing_scores(queried_race)
+    filtered_math_scores = retrieve_race_based_math_scores(queried_race)
 
     mapped_reading = filtered_reading_scores.map { |score| {score[:timeframe] => {reading: score[:data]}} }
     mapped_math = filtered_math_scores.map { |score| {score[:timeframe] => {math: score[:data]}} }
@@ -83,6 +102,8 @@ class StatewideTesting
 
   def proficient_for_subject_by_grade_in_year(subject, grade, year)
     # The method returns a truncated three-digit floating point number representing a percentage.
+    confirm_grade_data(grade)
+    confirm_subject_data(subject)
     parsed_file = parser(choose_data_for_testing_scores(grade))
     grouped_data_by_subject = parsed_file.group_by {|hsh| hsh.fetch(:score).to_sym.downcase}
     queried_subject_data = grouped_data_by_subject[subject].group_by {|hsh| hsh.fetch(:timeframe).to_i}
@@ -92,24 +113,15 @@ class StatewideTesting
   def proficient_for_subject_by_race_in_year(subject, queried_race, year)
     # This method take three parameters: subject as symbol; race as symbol; year as integer
     # The method returns a truncated three-digit floating point number representing a percentage.
-    input = create_case_insensitive_symbol(queried_race)
-    mapped = proficient_by_race_or_ethnicity(input)
+    confirm_race_data(queried_race)
+    confirm_subject_data(subject)
+    mapped = proficient_by_race_or_ethnicity(queried_race)
     mapped.fetch(year)[subject]
   end
 
   def proficient_for_subject_in_year(subject, year)
   # This method take two parameters: subject as symbol; year as integer
-    proficient_for_subject_by_race_in_year(subject, queried_race="All Students", year)
-
-  # subject as a symbol from the following set: [:math, :reading, :writing]
-  # year as an integer for any year reported in the data
-  # A call to this method with any invalid parameter (like subject of :history) should raise a UnknownDataError.
-  #
-  # The method returns a truncated three-digit floating point number representing a percentage.
-  #
-  # Example:
-  #
-  # statewide_testing.proficient_for_subject_in_year(:math, 2012) # => 0.680
+    proficient_for_subject_by_race_in_year(subject, :all_students, year)
   end
 
 end
--- test/test_statewide_testing.rb
+++ test/test_statewide_testing.rb
@@ -12,6 +12,13 @@ class TestStatewideTesting < Minitest::Test
   #   assert_equal 0.862, district.statewide_testing.proficient_by_grade(3).fetch(2009)[:reading]
   # end
   #
+  # def test_proficient_by_grade_data_error
+  #   path = File.expand_path("../data", __dir__) # __dir__ means the directory this file is currently in. And __file__ is the current file.
+  #   repository = DistrictRepository.from_csv(path) # repository almost means a search engine
+  #   district = repository.find_by_name("ACADEMY 20") # can make header in CSV file the keys of the hashes
+  #   assert_raises(UnknownDataError) {district.statewide_testing.proficient_by_grade(7).fetch(2009)[:reading]}
+  # end
+  #
   # def test_proficient_by_grade_diff_district
   #   path = File.expand_path("../data", __dir__) # __dir__ means the directory this file is currently in. And __file__ is the current file.
   #   repository = DistrictRepository.from_csv(path) # repository almost means a search engine
@@ -39,6 +46,21 @@ class TestStatewideTesting < Minitest::Test
   #   district = repository.find_by_name("ACADEMY 20") # can make header in CSV file the keys of the hashes
   #   assert_equal 0.424, district.statewide_testing.proficient_by_race_or_ethnicity(:black)[2011][:math]
   # end
+
+  # def test_proficient_by_race_or_ethnicity_two_words
+  #   path = File.expand_path("../data", __dir__) # __dir__ means the directory this file is currently in. And __file__ is the current file.
+  #   repository = DistrictRepository.from_csv(path) # repository almost means a search engine
+  #   district = repository.find_by_name("ACADEMY 20") # can make header in CSV file the keys of the hashes
+  #   assert_equal 0.614, district.statewide_testing.proficient_by_race_or_ethnicity(:native_american)[2011][:math]
+  # end
+
+  #
+  # def test_proficient_by_race_or_ethnicity_invalid_race_error
+  #   path = File.expand_path("../data", __dir__) # __dir__ means the directory this file is currently in. And __file__ is the current file.
+  #   repository = DistrictRepository.from_csv(path) # repository almost means a search engine
+  #   district = repository.find_by_name("ACADEMY 20") # can make header in CSV file the keys of the hashes
+  #   assert_raises(UnknownRaceError) {district.statewide_testing.proficient_by_race_or_ethnicity(:red)}
+  # end
   #
   # def test_proficient_by_race_or_ethnicity_diff_ethnicity_diff_subject
   #   path = File.expand_path("../data", __dir__) # __dir__ means the directory this file is currently in. And __file__ is the current file.
@@ -54,25 +76,60 @@ class TestStatewideTesting < Minitest::Test
   #   assert_equal 0.849, district.statewide_testing.proficient_for_subject_by_grade_in_year(:math, 3, 2010)
   # end
   #
+  # def test_proficient_for_subject_by_grade_in_year_invalid_subject
+  #   path = File.expand_path("../data", __dir__) # __dir__ means the directory this file is currently in. And __file__ is the current file.
+  #   repository = DistrictRepository.from_csv(path) # repository almost means a search engine
+  #   district = repository.find_by_name("ACADEMY 20") # can make header in CSV file the keys of the hashes
+  #   assert_raises(UnknownDataError) {district.statewide_testing.proficient_for_subject_by_grade_in_year(:science, 8, 2013)}
+  # end
+  #
+  # def test_proficient_for_subject_by_grade_in_year_invalid_grade
+  #   path = File.expand_path("../data", __dir__) # __dir__ means the directory this file is currently in. And __file__ is the current file.
+  #   repository = DistrictRepository.from_csv(path) # repository almost means a search engine
+  #   district = repository.find_by_name("ACADEMY 20") # can make header in CSV file the keys of the hashes
+  #   assert_raises(UnknownDataError) {district.statewide_testing.proficient_for_subject_by_grade_in_year(:math, 7, 2013)}
+  # end
+  #
   # def test_proficient_for_subject_by_grade_in_year_diff_district
   #   path = File.expand_path("../data", __dir__) # __dir__ means the directory this file is currently in. And __file__ is the current file.
   #   repository = DistrictRepository.from_csv(path) # repository almost means a search engine
   #   district = repository.find_by_name("KIOWA C-2") # can make header in CSV file the keys of the hashes
   #   assert_equal 0.24, district.statewide_testing.proficient_for_subject_by_grade_in_year(:writing, 3, 2011)
   # end
+  #
+  # def test_proficient_for_subject_by_race_in_year
+  #   path = File.expand_path("../data", __dir__) # __dir__ means the directory this file is currently in. And __file__ is the current file.
+  #   repository = DistrictRepository.from_csv(path) # repository almost means a search engine
+  #   district = repository.find_by_name("ACADEMY 20") # can make header in CSV file the keys of the hashes
+  #   assert_equal 0.805, district.statewide_testing.proficient_for_subject_by_race_in_year(:math, :asian, 2013)
+  # end
+  #
+  # def test_proficient_for_subject_by_race_in_year_no_data
+  #   path = File.expand_path("../data", __dir__) # __dir__ means the directory this file is currently in. And __file__ is the current file.
+  #   repository = DistrictRepository.from_csv(path) # repository almost means a search engine
+  #   district = repository.find_by_name("KIOWA C-2") # can make header in CSV file the keys of the hashes
+  #   assert_equal 0.0, district.statewide_testing.proficient_for_subject_by_race_in_year(:math, :asian, 2013)
+  # end
+  #
+  # def test_proficient_for_subject_by_race_in_year_invalid_subject
+  #   path = File.expand_path("../data", __dir__) # __dir__ means the directory this file is currently in. And __file__ is the current file.
+  #   repository = DistrictRepository.from_csv(path) # repository almost means a search engine
+  #   district = repository.find_by_name("KIOWA C-2") # can make header in CSV file the keys of the hashes
+  #   assert_raises(UnknownDataError) {district.statewide_testing.proficient_for_subject_by_race_in_year(:science, :asian, 2013)}
+  # end
 
-  def test_proficient_for_subject_by_race_in_year
+  def test_proficient_for_subject_by_race_in_year_pacific_islander
     path = File.expand_path("../data", __dir__) # __dir__ means the directory this file is currently in. And __file__ is the current file.
     repository = DistrictRepository.from_csv(path) # repository almost means a search engine
-    district = repository.find_by_name("ACADEMY 20") # can make header in CSV file the keys of the hashes
-    assert_equal 0.805, district.statewide_testing.proficient_for_subject_by_race_in_year(:math, :asian, 2013)
+    district = repository.find_by_name("KIOWA C-2") # can make header in CSV file the keys of the hashes
+    assert_equal "", district.statewide_testing.proficient_for_subject_by_race_in_year(:math, :pacific_islander, 2013)
   end
 
-  def test_proficient_for_subject_in_year
-    path = File.expand_path("../data", __dir__) # __dir__ means the directory this file is currently in. And __file__ is the current file.
-    repository = DistrictRepository.from_csv(path) # repository almost means a search engine
-    district = repository.find_by_name("ACADEMY 20") # can make header in CSV file the keys of the hashes
-    assert_equal "0.805", district.statewide_testing.proficient_for_subject_in_year(:math, 2013)
-  end
+  # def test_proficient_for_subject_in_year
+  #   path = File.expand_path("../data", __dir__) # __dir__ means the directory this file is currently in. And __file__ is the current file.
+  #   repository = DistrictRepository.from_csv(path) # repository almost means a search engine
+  #   district = repository.find_by_name("ACADEMY 20") # can make header in CSV file the keys of the hashes
+  #   assert_equal 0.689, district.statewide_testing.proficient_for_subject_in_year(:math, 2012)
+  # end
 
 end
