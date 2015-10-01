9750bb4498d111b2901a6889d7046ff623602e78
Amber Crawford <ammcrawford@gmail.com>
2015-09-30 20:02:26 -0600

    Added testing for District Repo

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -5,28 +5,19 @@ class DistrictRepository
 
   attr_accessor :repository
 
-  def initialize
-  end
-
   def self.from_csv(path)
-    # opens one csv in folder
     filename = "Students qualifying for free or reduced price lunch.csv"
     fullpath = File.join(path, filename)
     @repository = {}
-    # pushes it into parser
     rows = CSV.read(fullpath, headers: true, header_converters: :symbol).each do |row|
-      # only push unique names into our hash and have them point to its instance of District
       if !@repository.include?(row[:location])
         @repository[row[:location].upcase] = District.new(row[:location].upcase)
       end
     end
-    # returns hash containing just district names as strings
-    # binding.pry
     self
   end
 
   def self.find_by_name(name)
-    # searches returned hash above for district name; receives string; - returns instance of district
     if @repository.keys.include? name.upcase
       return @repository[name.upcase]
     else
@@ -35,7 +26,6 @@ class DistrictRepository
   end
 
   def self.find_all_matching(text)
-    # find_all_matching - returns either [] or one or more matches which contain the supplied name fragment, case insensitive
     @repository.select{|k, v|
       if k.include?(text.upcase)
         v
@@ -45,7 +35,6 @@ class DistrictRepository
 
   def self.repository_holder
     @repository
-
   end
 
 end
